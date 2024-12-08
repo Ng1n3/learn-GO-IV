8a4205fb961b02db3a1ca0987bf49cdc79f96d93
ng1n3 <code.with.muyiwa@gmail.com>
2024-12-08 17:15:44 +0100

    composite

--- composite/main.go
+++ composite/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"maps"
 	"slices"
 )
 
@@ -61,7 +62,7 @@ func main() {
 
 	//! -> Avoid combining append wth slicing slices
 
-	// [lecture] -> This means elements of the original slice beyond the end of the subslice, including unused capacity, are shared by both slices.
+	// [lecture] -> This means elements of the original slice beyond the end of the subslice, including unused capacity, are shared by both slices. You can also slice arrays too.
 
 	w := make([]string, 0, 5)
 	w = append(w, "a", "b", "c", "d")
@@ -75,12 +76,189 @@ func main() {
 	fmt.Println("z: ", z)
 	fmt.Println()
 
+	fmt.Println("------COPY------")
+	//[lecture] -> COPY used to create a slice that is independent of the original. copy(destination slice, source slice)
 
-  fmt.Println("------COPY------")
-  //[lecture] -> COPY used to create a slice that is independent of the original. copy(destination slice, source slice)
+	a := []int{1, 2, 3, 4}
+	b := make([]int, 4)
+	num := copy(b, a)
+	fmt.Println(b, num)
+	fmt.Println()
 
-  a := []int{1, 2, 3, 4}
-  b := make([]int, 4)
-  num := copy(b, a)
-  fmt.Println(b, num)
+	fmt.Println("---converting arrays to slices----")
+	// ? -> Taking a slice from an array has the same memory-sharing properties as taking a slice from a slice.
+	c := [4]int{5, 6, 7, 8}
+	d := c[:2]
+	e := c[2:]
+
+	fmt.Println("c: ", c)
+	fmt.Println("d: ", d)
+	fmt.Println("e: ", e)
+	fmt.Println()
+
+	fmt.Println("-----Converting Slices to Arrays-----")
+	xSlice := []int{1, 2, 3, 4}
+	xArray := [4]int(xSlice)
+	smallArray := [2]int(xSlice)
+	xSlice[0] = 14
+	fmt.Println("xSLice: ", xSlice)
+	fmt.Println("xArray: ", xArray)
+	fmt.Println("smallArray: ", smallArray)
+	fmt.Println()
+
+	fmt.Println("-----Strings and Runes and Bytes------")
+	var s1 string = "Hello 🌑"
+	var s2 string = s1[4:7]
+	var s3 string = s1[:5]
+	var s4 string = s1[6:]
+	fmt.Println("s1: ", s1)
+	fmt.Println("s2: ", s2)
+	fmt.Println("s3: ", s3)
+	fmt.Println("s4: ", s4)
+	fmt.Println(len(s1))
+	fmt.Println()
+
+	fmt.Println("-----MAPS-----")
+	f := map[string]int{
+		"hello": 9,
+		"world": 15,
+		"!":     10,
+		"...":   20,
+	}
+
+	g, ok := f["hello"]
+	fmt.Println(g, ok)
+	h, ok := f["baddie"]
+	fmt.Println(h, ok)
+
+	// ? DELETING FROM MAPS
+	delete(f, "hello")
+	i, ok := f["hello"]
+	fmt.Println(i, ok)
+
+	// ? EMPTYING MAPS
+	clear(f)
+	fmt.Println(f, len(f))
+
+	// ? COMPARING MAPS
+	j1 := map[string]int{
+		"hello": 9,
+		"world": 15,
+		"!":     10,
+		"...":   20,
+	}
+
+	j2 := map[string]int{
+		"hello": 9,
+		"world": 15,
+		"!":     10,
+		"...":   20,
+	}
+	fmt.Println(maps.Equal(j1, j2))
+
+	// ? USING MAPS AS SETS
+	intSet := map[int]bool{}
+	vals := []int{5, 10, 5, 20, 5, 45, 100, 1, 2}
+	for _, v1 := range vals {
+		intSet[v1] = true
+	}
+
+	fmt.Println(len(vals), len(intSet))
+	fmt.Println(intSet[5])
+	fmt.Println(intSet[500])
+	fmt.Println()
+
+	fmt.Println("-----STRUCTS-----")
+	type person struct {
+		name string
+		age  int
+		car  string
+	}
+
+	var emmanuel person
+
+	emmanuel = person{
+		"emmanuel",
+		26,
+		"Camry",
+	}
+
+	fmt.Println(emmanuel)
+
+	// ? ANONYMOUS STRUCTS
+	car := struct {
+		name string
+		year int
+	}{
+		name: "Toyota Camry",
+		year: 2016,
+	}
+
+	fmt.Println(car.name, car.year)
+
+	// ! -> Go doesn't allow comparison between variables that represent structs of different types
+
+	type firstPerson struct {
+		name string
+		age  int
+	}
+
+	f1 := firstPerson{
+		name: "Emmanuel",
+		age:  26,
+	}
+
+	type secondPerson struct {
+		name string
+		age  int
+	}
+
+	f2 := secondPerson{
+		name: "Daniella",
+		age:  26,
+	}
+
+	//? --> you can use a type conversionto convert te instance of firstPerson to secondPerson, but you cannot use == to compare the instance of firstPerson and an instance of secondPerson, because they are of different types.
+	// fmt.Println("comparing f1 and f2", f1 == f2)
+	f2 = secondPerson(f1)
+	fmt.Println(f2, f1);
+	
+
+	type thirdPerson struct {
+		age  int
+		name string
+	}
+
+	f3 := thirdPerson {
+		age: 26,
+		name: "Emmanuel",
+	}
+
+	// ? --> you can't convert an instance of first person to thirdPerson, because the field are in different order
+
+	type fourthPerson struct {
+		firstName string
+		age       int
+	}
+
+	f4 := fourthPerson {
+		firstName: "Emmanuel",
+		age: 26,
+	}
+
+	// ? --> you cannot convert an instance of firstPerson to fourthPerson because the names don't match
+
+	type fifthPerson struct {
+		name          string
+		age           int
+		favoritecolor string
+	}
+	f5 := fifthPerson {
+		name: "Emmanuel",
+		age: 26,
+		favoritecolor: "Red",
+	}
+
+	// ? --> you cannot convert an instance of firstPerson to fifthPerson because there is an additional field
+
 }
